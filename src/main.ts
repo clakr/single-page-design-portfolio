e748216004bd597a322c125577cbef34a90a9a60
clakr <clarktolosa@gmail.com>
2023-11-08 20:27:55 +0800

    mobile layout and assets

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,50 @@ function changeTextContent(element: HTMLTitleElement | Element | null) {
   element.textContent = import.meta.env.VITE_PROJECT_NAME;
 }
 
-(function () {
+const SLIDE_WIDTH = 270;
+
+document.addEventListener("DOMContentLoaded", function () {
   const title = document.querySelector("title");
   changeTextContent(title);
 
   const heading = document.querySelector(".sr-only");
   changeTextContent(heading);
-})();
+
+  let slideIndex = 2;
+
+  const slider = document.querySelector("ul.wrapper--slides");
+  if (!slider) return;
+
+  slider.scrollTo({
+    left: slideIndex * SLIDE_WIDTH,
+    behavior: "instant",
+  });
+
+  const nextButton = document.querySelector("li[data-navigation=nextSlide]");
+  const prevButton = document.querySelector("li[data-navigation=prevSlide]");
+  if (!nextButton || !prevButton) return;
+
+  nextButton.addEventListener("click", () => {
+    const slidesCount = slider.childElementCount;
+
+    if (slideIndex !== slidesCount) {
+      slideIndex++;
+    }
+
+    slider.scrollTo({
+      left: slideIndex * SLIDE_WIDTH,
+      behavior: "smooth",
+    });
+  });
+
+  prevButton.addEventListener("click", () => {
+    if (slideIndex !== 0) {
+      slideIndex--;
+    }
+
+    slider.scrollTo({
+      left: slideIndex * SLIDE_WIDTH,
+      behavior: "smooth",
+    });
+  });
+});
